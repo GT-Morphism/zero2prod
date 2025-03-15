2564469a9ae319bebad017d5f6c9bb819e043b90
Giuseppe Turturiello <giuseppe-pio@hotmail.de>
2025-03-15 02:04:38 +0100

    chore: add dynamic port to health check test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -2,12 +2,11 @@ use std::net::SocketAddr;
 
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app().await;
-
+    let address = spawn_app().await;
     let client = reqwest::Client::new();
 
     let response = client
-        .get("http://127.0.0.1:3000/health_check")
+        .get(format!("{}/health_check", address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -16,16 +15,14 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-async fn spawn_app() {
-    let port: u16 = std::env::var("PORT")
-        .unwrap_or_else(|_| "3000".to_string())
-        .parse()
-        .expect("Failed to parse PORT");
-
-    let address = SocketAddr::from(([0, 0, 0, 0], port));
+async fn spawn_app() -> String {
+    let address = SocketAddr::from(([0, 0, 0, 0], 0));
     let listener = tokio::net::TcpListener::bind(&address).await.unwrap();
+    let port = listener.local_addr().unwrap().port();
 
     tokio::spawn(async move {
         axum::serve(listener, zero2prod::app()).await.unwrap();
     });
+
+    format!("http://127.0.0.1:{}", port)
 }
